03801a38d27235a9482c4323305f64cf273d8ca7
Ales Povalac <alpov@alpov.net>
2022-09-20 13:11:42 +0200

    blikani ledkou

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -17,13 +17,16 @@
  */
 
 #include <stdint.h>
-
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include "stm32f0xx.h"
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+	/* Loop forever */
+	for (;;) {
+		GPIOA->ODR ^= (1<<5); // toggle
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+	}
 }
