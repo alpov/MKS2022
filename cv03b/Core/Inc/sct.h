654fcf7e6b50d67edcd9febe9b5a61e193766a83
Ales Povalac <alpov@alpov.net>
2022-10-11 15:02:34 +0200

    cv03, cv03b

---
+++ cv03b/Core/Inc/sct.h
@@ -0,0 +1,16 @@
+
+/*
+ * sct.h
+ *
+ *  Created on: Oct 11, 2022
+ *      Author: povalac
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t led);
+void sct_value(uint16_t value);
+
+#endif /* SCT_H_ */
